7d1b797369627dc5652d1760e64e42232b8e545c
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2019-04-04 19:01:14 -0500

    m time

--- funct.hs
+++ funct.hs
@@ -1,3 +1,7 @@
+import Text.Printf
+import Control.Exception
+import System.CPUTime
+
 imprime x | x == 1 = "1"
           | otherwise = (show x) ++ " "++imprime (x-1)
 
@@ -25,3 +29,28 @@ binary  list val izq der | list == [] = (-1)
                          | list !! medio > val = binary list val izq (medio)
                          | list !! medio < val = binary list val (medio+1) der
                          where medio = (div (izq+der) 2)
+
+ternary  list val low hi | list == [] = (-1)
+                         | hi < low = (-1)
+                         | list !! (fst medio) == val = (fst medio)
+                         | list !! (snd medio) == val = (snd medio)
+                         | list !! (fst medio) > val = ternary list val (low) ((fst medio) -1)
+                         | list !! (fst medio) < val && list !! (snd medio) > val = ternary list val ((fst medio)+1) ((snd medio)-1)
+                         | list !! (snd medio) < val = ternary list val ((snd medio)+1) (hi-1)
+                         where medio = (low+(div (hi-low) 3), (hi-(div (hi-low) 3)))
+
+
+time :: IO t -> IO t
+time a = do
+    start <- getCPUTime
+    v <- a
+    x = ternary [1..10] 1 0 10
+    end   <- getCPUTime
+    let diff = (fromIntegral (end - start)) / (10^12)
+    printf "Computation time: %0.3f sec\n" (diff :: Double)
+    return v
+
+--main = do
+--    putStrLn "Starting..."
+  --  time $ product [1..10000] `seq` return ()
+    --putStrLn "Done."
