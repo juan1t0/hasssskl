657a8bb012359a3dd6b9644096ed3cf2a32cec4a
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2019-10-24 18:56:00 -0500

    casi, faltan contextos

--- compilers/practica5.cpp
+++ compilers/practica5.cpp
@@ -1,3 +1,6 @@
+/**
+* Hecho por Heredia Parillo, JuanPablo Andrew
+**/
 #include <iostream>
 #include <vector>
 #include <set>
@@ -14,7 +17,7 @@ using namespace std;
 void DeteleFromString(string &s, char d){
 	string result;
 	for(size_t i = 0; i < s.size(); ++i){
-        if(s[i] == d)continue;
+		if(s[i] == d)continue;
 		result += s[i];
 	}
 	s = result;
@@ -33,7 +36,7 @@ void printMatrix(vector<vector<T>> &mat){
 class Produccion{
 public:
 	string nombre;
-	vector<string> *der;
+	vector<string> *der=0;
 public:
 	Produccion(){}
 	Produccion(string &s): nombre(s){}
@@ -47,8 +50,8 @@ public:
 
 class ContexProduc : public Produccion{
 public:
-	vector<contex>* contexto;
-	vector<Produccion>* derP;
+	vector<contex>* contexto=0;
+	vector<Produccion>* derP=0;
 	ContexProduc(Produccion p):Produccion(p.nombre,*(p.der)){
 		cout<<"aqa"<<endl;
 		contexto = 0;
@@ -73,12 +76,12 @@ struct Gramatica{
 	vector<Produccion*> production;
 	set<string> terminales;
 	set<string> noterminales;
-
+	
 	void read(string texto){
-        if(texto.find("=?")){
-            readContx(texto);
-            return;
-        }
+		if(texto.find("=?")){
+			readContx(texto);
+			return;
+		}
 		string temp1 = texto.substr(0, texto.find( ":=" ));
 		DeteleFromString(temp1,' ');
 		string temp2 = texto.substr(texto.find(":=") + 2, texto.length() - 1);
@@ -95,11 +98,10 @@ struct Gramatica{
 			production.push_back(temp);
 		}			
 	}
-    void readContx(string texto){
+	void readContx(string texto){
 		short separe = texto.find(":=");
 		string nameIZQ = texto.substr(0,texto.find("["));
 		DeteleFromString(nameIZQ,' ');
-		//cout << "|" << nameIZQ << "|" << endl;
 		vector<contex> tempContx = readVarContx(texto.substr(texto.find("[")+1,separe-texto.find("[")-1));
 		string dere = texto.substr(separe+2);
 		if(dere.find("?_") != string::npos){
@@ -111,9 +113,8 @@ struct Gramatica{
 			ContexProduc * ctp = new ContexProduc(nameIZQ,tempContx,to_der);
 			production.push_back(ctp);
 		}
-    }
+	}
 	vector<contex> readVarContx(string subtex){
-//		DeteleFromString(subtex,'[');
 		DeteleFromString(subtex,']');
 		stringstream Saux(subtex);
 		string temp;
@@ -121,9 +122,7 @@ struct Gramatica{
 		while(getline(Saux, temp, ',')){
 			contex a = make_pair(temp.substr(0,temp.find("=")),temp.substr(temp.find("=")+1));
 			tempContx.push_back(a);
-	//		cout << "<" << a.first << "," << a.second << "> " ;
 		}
-	//	cout << endl;
 		return tempContx;
 	}
 	vector<Produccion> readMoreProduc(string &subtex){
@@ -134,10 +133,8 @@ struct Gramatica{
 			string nameIZQ = temp.substr(0,temp.find("["));
 			vector<contex> tempContx = readVarContx(temp.substr(temp.find("[")+1));
 			otherProd.push_back(ContexProduc(nameIZQ,tempContx));
-//			production.push_back(& otherProd[otherProd.size()-1]);
-	//		cout << ">>" << nameIZQ << ": " << tempContx.size() << " <<";
+			//			production.push_back(& otherProd[otherProd.size()-1]);
 		}
-	//	cout << endl;
 		return otherProd;
 	}
 	vector<string> readTerminals(string &subtex){
@@ -146,33 +143,63 @@ struct Gramatica{
 		vector<string> terminals;
 		while(Saux>>temp){
 			terminals.push_back(temp);
-		//	cout << "||" << temp << " ; " ;
 		}
-	//	cout << "||" << endl;
 		return terminals;
 	}
 	vector<vector<string>> getProduction(string izq){
 		vector<vector<string>> result;
 		for(size_t i = 0; i < production.size(); ++i){
 			if(production[i]->nombre == izq){
-				if(production[i]->der == 0){
-					//cout<<"algo"<<endl;
+				if(production[i]->der){
+					result.push_back(*(production[i]->der));
+				}
+				else{
 					vector<string> temp;
-					//cout<<"algito"<<endl;
 					ContexProduc *a = (ContexProduc *)(production[i]);
-					//cout<<a->nombre<<endl;
 					for(int j=0;j < a->derP->size();j++){
 						string te = a->derP->at(j).nombre;
 						temp.push_back(te);
 					}	
-					result.push_back(temp);				
-				}
-				else{
-					result.push_back(*(production[i]->der));
+					result.push_back(temp);
 				}
 			}
 		}
 		return result;
+	}
+	void printGrammar(){
+		for(size_t i = 0; i < production.size(); ++i){
+			cout << production[i]->nombre;
+			ContexProduc *a = (ContexProduc *)(production[i]);
+			if(!a->contexto){
+				cout << " :: ";
+				for(int j =0;j < production[i]->der->size();j++){
+					cout << production[i]->der->at(j) << "\t";
+				}
+			}
+			else{
+				ContexProduc *a = (ContexProduc *)(production[i]);
+				cout<< "<";
+				for(int j=0;j < a->contexto->size();j++){
+					cout<<a->contexto->at(j).first <<"  ";
+				}
+				cout << "> :: ";
+				if(!a->derP){
+					for(int j =0;j < production[i]->der->size();j++){
+						cout << production[i]->der->at(j) << "\t";
+					}
+				}else{
+					for(int j=0;j < a->derP->size();j++){
+						cout<<a->derP->at(j).nombre<< "<";
+						/*ContexProduc *b = (ContexProduc *)&(a->derP->at(j));
+						for(int k=0;k<b->contexto->size();k++){
+							cout<< b->contexto->at(k).first<<" ";
+						}*/
+						cout<<">\t";
+					}	
+				}
+			}
+			cout<<endl;
+		}
 	}
 	Gramatica(){}
 };
@@ -240,35 +267,40 @@ public:
 
 int main(int argc, char *argv[]) {
 	Gramatica grammar;
-/*	grammar.read("E := T Ep");
+	/*	grammar.read("E := T Ep");
 	grammar.read("Ep := + T Ep | - T Ep | lambda");
 	grammar.read("T := F Tp");
 	grammar.read("Tp := * F Tp");
 	grammar.read("F := (E) | num | id");*/
-
-	grammar.read("Sus[gen=?_] := Det[gen=?_] Suj[gen=?_]");
+	
+	grammar.read("Sus[gen=?_,num=?_] := Det[gen=?_] Suj[gen=?_,num=?_]");
 	grammar.read("Det[gen=?f] := 'la'");
-/*	grammar.read("Det[gen=?m] := 'el'");
+	grammar.read("Det[gen=?m] := 'el'");
 	grammar.read("Suj[gen=?m] := 'nino'");
 	grammar.read("Suj[gen=?f] := 'nina'");
-*/
+	/*
 	vector<vector<string>> producciones = grammar.getProduction("Sus");
 	printMatrix(producciones);
-
+	producciones = grammar.getProduction("Det");
+	printMatrix(producciones);
+	producciones = grammar.getProduction("Suj");
+	printMatrix(producciones);*/
+	grammar.printGrammar();
+	
 	return 0;
 }
 
 /**
- * Sus[gen=? ] := Det[gen=? ] Suj[gen=? ]
- * Det[gen=f] := 'la'
- * Det[gen=m] := 'el'
- * Suj[gen=m] := 'nino'
- * Suj[gen=f] := 'nina'
+* Sus[gen=? ] := Det[gen=? ] Suj[gen=? ]
+* Det[gen=f] := 'la'
+* Det[gen=m] := 'el'
+* Suj[gen=m] := 'nino'
+* Suj[gen=f] := 'nina'
 */
 
 /*	AnalizadorSintacticoLL1 analizador;
-	analizador.llenarTas();
-	cout<<analizador.reconocerSentencia("num + num + num + num")<<endl;
-	cout<<analizador.reconocerSentencia("( num + num ) + ( num + num )")<<endl;	
-	cout<<analizador.reconocerSentencia("num * ( num * num )")<<endl;	
-	cout<<analizador.reconocerSentencia("( num *) num")<<endl;	*/
+analizador.llenarTas();
+cout<<analizador.reconocerSentencia("num + num + num + num")<<endl;
+cout<<analizador.reconocerSentencia("( num + num ) + ( num + num )")<<endl;	
+cout<<analizador.reconocerSentencia("num * ( num * num )")<<endl;	
+cout<<analizador.reconocerSentencia("( num *) num")<<endl;	*/
